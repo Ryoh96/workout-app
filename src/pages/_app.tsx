0e9ca1f29fe977bf8bb5609e1df423fe2f795d67
Ryoh96 <account@kurokiryoh.com>
2023-07-19 19:23:09 +0900

    Removed loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,38 +19,12 @@ export const client = new ApolloClient({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  const [pageLoading, setPageLoading] = useState(false)
-
-  useEffect(() => {
-    const handleStart = (url: string) =>
-      url !== router.asPath && setPageLoading(true)
-    const handleComplete = () => setPageLoading(false)
-
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleComplete)
-    router.events.on('routeChangeError', handleComplete)
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleComplete)
-      router.events.off('routeChangeError', handleComplete)
-    }
-  })
-
   return (
     <RecoilRoot>
       <SessionProvider session={pageProps.session}>
         <ApolloProvider client={client}>
           <SkeletonTheme baseColor="#2020202" highlightColor="#444">
             <Layout>
-              {pageLoading && (
-                <div className="text-white font-bold  fixed h-screen w-screen bg-black bg-opacity-30 top-0 left-0 flex items-center justify-center z-[99999]">
-                  <div>
-                    <Spinner />
-                  </div>
-                </div>
-              )}
               <Component {...pageProps} />
             </Layout>
           </SkeletonTheme>
